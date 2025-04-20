3c35a74d24fbc3d6ab7c66d11a3672cabbf3ea39
Saurab Ghimire <sourrab@gmail.com>
2025-04-20 16:23:26 -0500

    s3 bucket changed

--- scripts/upload-to-s3.sh
+++ scripts/upload-to-s3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-S3_BUCKET_NAME=saurab.ghimire.cloud.computing.react-codepipeline
+S3_BUCKET_NAME=saurab-ghimire-react-codepipeline-s3
 
 echo "Uploading build files to S3 bucket: $S3_BUCKET_NAME"
 aws s3 sync /tmp/react-build/ s3://$S3_BUCKET_NAME/ --delete
